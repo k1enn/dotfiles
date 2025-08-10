0dcf57d435b4cb9d7d046737ffe05a1dfc594de4
k1enn <dinhtrungkien05@gmail.com>
2025-08-10 14:47:12 +0700

    init

--- config/.config/sway/scripts/idle.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-exec swayidle -w \
-    timeout 300 'swaylock -f -c 000000' \
-    timeout 310 'swaymsg "output * dpms off"' \
-    resume 'swaymsg "output * dpms on"' \
-    before-sleep 'swaylock -f -c 000000'
--- zprofile/.zprofile
+++
@@ -1,4 +0,0 @@
-# Auto-launch Sway on tty1 login
-if [ -z "$WAYLAND_DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
-    exec dbus-run-session sway --unsupported-gpu
-fi
