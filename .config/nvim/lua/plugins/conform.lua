6e11eca8695c636fa45e71bdf45669706a81fa64
k1enn <dinhtrungkien05@gmail.com>
2025-09-29 11:06:38 +0700

    add: nvim conform specific format languages

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,21 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        json = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        markdown = { "prettier" },
+        csharp = { "prettier" },
+      },
+    })
+  end,
+}
